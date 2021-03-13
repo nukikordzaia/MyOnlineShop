bb3df0a732c66cdb65d7caa758d9854e564d965c
nukikordzaia <nukikordzaia2000@gmail.com>
2021-03-14 00:32:38 +0400

    fixed serializers

--- shop/permissions.py
+++ shop/permissions.py
@@ -2,7 +2,8 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.owner == request.user
+    def has_permission(self, request, view):
+        if request.method not in permissions.SAFE_METHODS:
+            return request.user.is_staff
+        return True
+
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,16 @@
 import json
-
-from django.db.models import Sum, Q
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import  status
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import  permission_classes, action
-from shop.models import Category, Product, Cart, CartItem
+from rest_framework.decorators import action
+from shop.models import Category, Product, Cart
 from shop.permissions import IsAdmin
-from shop.serializers import CategorySerializer, ProductSerializer, ProductDetSerializer, CartSerializer, \
-    CartItemSerializer, CartItemDetSerializer
+from shop.serializers import CategorySerializer, ProductSerializer, ProductDetSerializer, CartSerializer,\
+    CartItemDetSerializer
 
 
 class CategoryList(ListCreateAPIView):
@@ -26,32 +24,31 @@ class ProductViewSet(ModelViewSet):
     serializer_classes = {
         'list': ProductSerializer,
         'retrieve': ProductDetSerializer,
+        'add_to_cart': CartItemDetSerializer,
     }
     default_serializer_class = ProductSerializer
+    permission_classes = [IsAdmin]
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['title', 'description']
     filterset_fields = ['category', 'price']
 
-    # @TODO permissioni rato ar mushaobs
-    @permission_classes([IsAdmin])
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.default_serializer_class)
 
-    @permission_classes([IsAdmin])
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-#@TODO shevamocmo slug
-    @action(detail=True, methods=['get', 'post'], permission_classes=[IsAdmin])
-    def addToCart(self, request):
-        serializer = CartItemDetSerializer(data=request.data)
-        return serializer
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated], url_path='addToCart')
+    def add_to_cart(self, request, **_):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(product=self.get_object(), cart_id=self.request.user.cart.pk)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CartViewSet(ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
-
 
     @action(methods=['post'], detail=False, permission_classes=[IsAuthenticated])
     def checkout(self, request):
@@ -65,11 +62,10 @@ class CartViewSet(ModelViewSet):
         }
 
         response = requests.post(
-            'https://hooks.slack.com/services/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',
+            'https://hooks.slack.com/services/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',
             headers=headers,
             data=json.dumps(data)
         )
-
 
         return Response(response, status=response.status_code)
 
